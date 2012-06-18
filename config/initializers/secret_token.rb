d6a305976668f5d6e23d639ea8dc964e5d685aaa
uplaabura <uplaabura@gmail.com>
2012-06-19 00:28:15 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tutoria::Application.config.secret_token = '5ea14078bc0d9482d2d50db47f79e220b42991efdacce161c6a17fa9dc231d0238439f6dd8d942f7349b489b679204b4a3a0eb1e10b916b6aa0a7d22c6a313f2'
